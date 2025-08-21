3c83f8558c2b676ec93689c4f662068392b215e5
onewancho <dwi.bagus.bahariawan@gmail.com>
2025-08-21 11:19:53 +0700

    update tugas

--- tugas2.php
+++ tugas2.php
@@ -3,12 +3,13 @@
 //Membuat Object dari barang sekitar kita
 
 class Tumbler{
-    function __construct(public string $color, public float $capacity, public string $material, public string $height, public string $brand) {}
+    function __construct(public string $color, public float $capacity, public string $material, public string $height, public string $brand, private string $owner) {}
 
-    public function fill(string $water) {
-        echo "Filling tumbler with $water";
+    public function getOwner() {
+        return $this->owner; 
     }
 }
 
-$tumbler = new Tumbler('Black', 500, 'Stainless Steel', '15cm', 'Hactiv8');
+$tumbler = new Tumbler('Black', 500, 'Stainless Steel', '15cm', 'Hactiv8', 'Wawan');
 print_r($tumbler);
+print_r($tumbler->getOwner());
